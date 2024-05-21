dcc7dc8f7bee37527fb9f7c794fcee6e245f0e20
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2024-05-21 17:17:42 +0200

    update parsing of params

--- swarmfish/utils.py
+++ swarmfish/utils.py
@@ -1,19 +1,41 @@
 from swarmfish.swarm_control import SwarmParams
 import yaml
 import json
+import logging
+
+log = logging.getLogger(__name__)
 
 def load_params_from_yaml(file_name: str) -> SwarmParams | None:
-    with open(file_name) as f:
-        yaml_string = f.read()
-        parsed = yaml.load(yaml_string, Loader=yaml.FullLoader)
-        print(parsed)
-        return SwarmParams(**parsed['Agent'])
+    try:
+        f = open(file_name)
+    except FileNotFoundError:
+        log.error(f'File {file_name} not found')
+    else:
+        try:
+            log.info('Parsing yaml config')
+            yaml_string = f.read()
+            parsed = yaml.load(yaml_string, Loader=yaml.FullLoader)
+            return SwarmParams(**parsed['Agent']['SwarmParams'])
+        except Exception as e:
+            log.error(f'Parsing error {e}')
+        finally:
+            f.close()
     return None
 
 def load_params_from_json(file_name: str) -> SwarmParams | None:
-    with open(file_name) as f:
-        json_string = f.read()
-        parsed = json.loads(json_string)
-        return SwarmParams(**parsed['Agent'])
+    try:
+        f = open(file_name)
+    except FileNotFoundError:
+        log.error(f'File {file_name} not found')
+    else:
+        try:
+            log.info('Parsing yaml config')
+            json_string = f.read()
+            parsed = json.loads(json_string)
+            return SwarmParams(**parsed['Agent']['SwarmParams'])
+        except Exception as e:
+            log.error(f'Parsing error {e}')
+        finally:
+            f.close()
     return None
 
